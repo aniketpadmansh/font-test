03ee04c2f61afda956d2a039de67680c8592d326
Aniket Padmansh <Aniket.padmansh@stockgro.com>
2024-06-27 19:18:09 +0530

    init

--- src/pages/font/index.js
+++ src/pages/font/index.js
@@ -4,6 +4,8 @@ const notoSans = Noto_Sans({
     weight: ['300', '400', '500', '600', '700'],
     subsets: ['latin'],
     variable: '--font-noto-sans',
+    preload: false,
+    display: 'swap'
 })
 
 const Font = () => {
